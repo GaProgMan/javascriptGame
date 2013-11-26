295a377887d5e0f38bddd7b2f822ec9e4ec6d925
GaProgMan <jamie@taylorj.org.uk>
2013-11-26 18:07:22 +0000

    basic score mode and killing character added

--- game.js
+++ game.js
@@ -35,6 +35,12 @@ var hero = {
 // Input handlers
 var keysDown = {};
 
+// Use to keep the score
+var score = 0;
+
+// Used to store whether the game is running or not
+var running = true;
+
 addEventListener("keydown", function(e){
 	// Capture a key down event (when the user presses a key)
 	keysDown[e.keyCode] = true;
@@ -53,47 +59,57 @@ var reset = function () {
 
 // Update the positions of all in game objects
 var update = function(deltaTime){
-	if (imageBackgroundReady && imageWarriorReady){
-		// Only do an actual update, if the images have been loaded.
-		if(38 in keysDown){ 		// Captured an "Up" arrow key press
-			hero.y -= hero.speed * deltaTime;
-			if (hero.y <= 0){
-				hero.y = 0;
+	if (running) {
+		// If the game is running, then update the objects on screen
+		if (imageBackgroundReady && imageWarriorReady){
+			// Only do an actual update, if the images have been loaded.
+			if(38 in keysDown){ 		// Captured an "Up" arrow key press
+				hero.y -= hero.speed * deltaTime;
+				if (hero.y <= 0){
+					hero.y = 0;
+				}
 			}
+			if(40 in keysDown){			// Captured a "Down" arrow key press
+				if (hero.y + imageWarrior.height >= gameCanvas.height){
+					// If the current position of hero plus his height already
+					// falls outside of the canvas, don't allow him to move)
+					hero.y = (gameCanvas.height - imageWarrior.height);
+				}
+				else{
+					// Otherwise move the hero as normal
+					hero.y += hero.speed * deltaTime;
+				}
+			}
+			if(37 in keysDown){			// Captured a "Left" arrow key press
+				hero.x -= hero.speed * deltaTime;
+				if (hero.x <= 0) {
+					// Don't allow the hero to go off the canvas (left-hand side)
+					hero.x = 1;
+				}
+			}
+			if(39 in keysDown){			// Captured a "Right arrow key press
+				if (hero.x + imageWarrior.width >= gameCanvas.width){
+					// If the current position of hero plus his width already
+					// falls outside of the canvas, don't allow him to move)
+					hero.x = (gameCanvas.width - imageWarrior.width);
+				}
+				else{
+					// Otherwise move the hero as normal
+					hero.x += hero.speed * deltaTime;
+				}
+			}
+			if (19 in keysDown){
+				running = false;
+			}
+			// Since the score is the number of seconds that the player has
+			// survived (i.e. not gotten caught by an enemy), we'll add the
+			// delta time number to it here
+			score+= deltaTime;
 		}
-		if(40 in keysDown){			// Captured a "Down" arrow key press
-			if (hero.y + imageWarrior.height >= gameCanvas.height){
-				// If the current position of hero plus his height already
-				// falls outside of the canvas, don't allow him to move)
-				hero.y = (gameCanvas.height - imageWarrior.height);
-			}
-			else{
-				// Otherwise move the hero as normal
-				hero.y += hero.speed * deltaTime;
-			}
+		else{
+			// Loading progress bar? Something to let the user know that
+			// we're loading the resources and will be back online soon
 		}
-		if(37 in keysDown){			// Captured a "Left" arrow key press
-			hero.x -= hero.speed * deltaTime;
-			if (hero.x <= 0) {
-				// Don't allow the hero to go off the canvas (left-hand side)
-				hero.x = 1;
-			}
-		}
-		if(39 in keysDown){			// Captured a "Right arrow key press
-			if (hero.x + imageWarrior.width >= gameCanvas.width){
-				// If the current position of hero plus his width already
-				// falls outside of the canvas, don't allow him to move)
-				hero.x = (gameCanvas.width - imageWarrior.width);
-			}
-			else{
-				// Otherwise move the hero as normal
-				hero.x += hero.speed * deltaTime;
-			}
-		}
-	}
-	else{
-		// Loading progress bar? Something to let the user know that
-		// we're loading the resources and will be back online soon
 	}
 };
 
@@ -111,8 +127,15 @@ var render = function() {
 		// Only draw the hero image if we could load it from storage
 		gameCanvasContext.drawImage(imageWarrior, hero.x, hero.y);
 	}
+	
+	// Score - number of seconds that the user has stayed alive
+	gameCanvasContext.fillStyle = "rgb(250, 250, 250)";
+	gameCanvasContext.font = "24px Helvetica";
+	gameCanvasContext.textAlign = "left";
+	gameCanvasContext.textBaseline = "top";
+	// Display the score (time alive) as a 2 decimal place number
+	gameCanvasContext.fillText("Score: " + score.toFixed(2), 32, 32);
 };
-
 
 // Main game loop
 var main = function(){
